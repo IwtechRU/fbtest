f685836de91fc54a66dfc0292f878bfd0bf03a3a
Nikita Menshutin <nikita.menshutin@ns-mediagroup.com>
2021-04-01 00:40:58 +0300

    RC?

--- src/modules/form.ts
+++ src/modules/form.ts
@@ -3,8 +3,11 @@ interface ajaxResponse { success: boolean, data: string };
 export default function ($class: string) {
     jQuery('body').on('submit', $class, function (event: Event) {
         event.preventDefault();
-        let formData: object = formDataGet(jQuery(this));
-        sendFormToBack(formData).then(gotResponse);
+        let $form: JQuery<HTMLElement> = jQuery(this);
+        let formData: object = formDataGet($form);
+        sendFormToBack(formData).then((data: ajaxResponse) => {
+            gotResponse(data, $form)
+        });
     });
     function sendFormToBack(data: object) {
         return new Promise<ajaxResponse>((resolve) => jQuery.ajax({
@@ -27,9 +30,12 @@ export default function ($class: string) {
         });
         return indexedArray;
     }
-    function gotResponse(value: ajaxResponse): void {
-        if (false===value.success) {
-
+    function gotResponse(value: ajaxResponse, form: JQuery<HTMLElement>): void {
+        form.find('.nfbpmessage').remove();
+        if (false === value.success) {
+            form.prepend('<div class="nfbpmessage nfbp-error">' + value.data + '</div>')
+        } else {
+            form.prepend('<div class="nfbpmessage nfbp-success">' + value.data + '</div>')
         }
     }
 }
